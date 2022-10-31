57a6253a4347074d8395bcaa0befeb2cc8e75c58
Chiranjeevi-TFP576 <p.chiranjeevi12345@gmail.com>
2022-10-31 20:53:45 +0530

    Mood Analyser

---
+++ Day20Assignment/MoodAnalyser.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day20ExceptionMoodResponse
+{
+    public class MoodAnalyser
+    {
+        public string message;
+        public  MoodAnalyser()//constuctor
+        {
+            this.message = "i am in SAD mood";
+
+        }
+
+        public MoodAnalyser(string message)
+        {
+            this.message = message.ToUpper();
+        }
+        public string AnalyseMood()
+        {
+
+            try
+            {
+                if (message.ToLower().Contains("sad"))
+                    return "SAD";
+
+
+                else return "HAPPY";
+            }
+            catch (NullReferenceException)
+            {
+
+                return "HAPPY";
+            }
+
+
+        }
+        public string AnalyseMoodWithoutConstructor(string message)
+        {
+            if (message.ToLower().Contains("sad"))
+                return "SAD";
+
+            else return "HAPPY";
+
+        }
+    }
+}
+
---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,105 @@
+using Day20ExceptionMoodResponse;
+
+namespace TestProject1
+{
+    public class Tests
+    {
+        MoodAnalyser moodAnalyser;
+        [SetUp]
+        public void Setup()
+        {
+            moodAnalyser = new MoodAnalyser();
+        }
+        /// <summary>
+        /// TC1.1 Given “I am in Sad Mood” message Should Return SAD
+        /// </summary>
+        [Test]
+        public void GivenMessage_WhenSad_ShouldReturnSadParameterLess_Constructor()
+        {
+            //Arrange
+            moodAnalyser = new MoodAnalyser();
+            string msg = "I am in SAD mood";
+            //Act
+            string message = moodAnalyser.AnalyseMoodWithoutConstructor(msg);
+            //Assert
+            Assert.AreEqual("SAD", message);
+        }
+        /// <summary>
+        /// TC1.2 Given “I am in Any Mood” message Should Return HAPPY
+        /// </summary>
+        [Test]
+        public void GivenMessage_WhenAnyMood_ShouldReturnHappyParameterLess_Constructor()
+        {
+            moodAnalyser = new MoodAnalyser();
+            string msg = "I am in Any mood";
+            string message = moodAnalyser.AnalyseMoodWithoutConstructor(msg);
+            Assert.AreEqual("HAPPY", message);
+        }
+        // <summary>
+        /// repeat TC1.1 Given “I am in SAD Mood” message in Constructor Should Return SAD
+        /// </summary>
+        [Test]
+        public void GivenMessage_WhenSad_ShouldReturnHappy()
+        {
+            moodAnalyser = new MoodAnalyser();
+            string message = moodAnalyser.AnalyseMood();
+            Assert.AreEqual("SAD", message);
+        }
+        // <summary>
+        /// repeat TC1.2 Given “I am in Happy Mood” message in Constructor Should Return HAPPY
+        /// </summary>
+        [Test]
+        public void GivenMessage_WhenHappy_ShouldReturnHappy()
+        {
+            moodAnalyser = new MoodAnalyser("I am in Happy mood");
+            string message = moodAnalyser.AnalyseMood();
+            Assert.AreEqual("HAPPY", message);
+        }
+        //UC-2
+        /// <summary>
+        /// TC2.1Given Null Mood Should Return Happy To make this Test Case pass Handle
+        /// NULL Scenario using try catch and return Happy
+        /// </summary>
+        [Test]
+        public void GivenMessage_WhenNull_ShouldReturnHappy()
+        {
+            moodAnalyser = new MoodAnalyser();
+            string message = moodAnalyser.AnalyseMood();
+            Assert.AreEqual("SAD", message);
+        }
+        // <summary>
+        /// tc 3.1 Given NULL Mood Should Throw MoodAnalysisException
+        /// </summary>
+        [Test]
+        public void GivenMessage_WhenNull_USingCustomException_ShouldReturnNullMood()
+        {
+            moodAnalyser = new MoodAnalyser();
+            try
+            {
+                string message = moodAnalyser.AnalyseMood();
+            }
+            catch (MoodAnalyserException exception)
+            {
+                Assert.AreEqual(MoodAnalyserException.ExceptionType.NULL_MOOD, exception.exceptionType);
+            }
+
+        }
+        // <summary>
+        /// tc 3.2 Given Empty Mood Should Throw MoodAnalysisException indicating Empty Mood
+        /// </summary>
+        [Test]
+        public void GivenMessage_WhenEmpty_USingCustomException_ShouldReturnNullMood()
+        {
+            moodAnalyser = new MoodAnalyser("");
+            try
+            {
+                string message = moodAnalyser.AnalyseMood();
+            }
+            catch (MoodAnalyserException exception)
+            {
+                Assert.AreEqual(MoodAnalyserException.ExceptionType.EMPTY_MOOD, exception.exceptionType);
+            }
+        }
+
+    }
+}
